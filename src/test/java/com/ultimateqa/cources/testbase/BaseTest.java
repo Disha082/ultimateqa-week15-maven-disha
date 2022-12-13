46434e479f0592bac4cb5d1b62fb2e15d48fe984
Disha082 <disha130308@gmail.com>
2022-12-13 11:52:13 +0000

    Initial commit

---
+++ src/test/java/com/ultimateqa/cources/testbase/BaseTest.java
@@ -0,0 +1,26 @@
+package com.ultimateqa.cources.testbase;
+
+
+
+import com.ultimateqa.cources.propertyreader.PropertyReader;
+import com.ultimateqa.cources.utility.Utility;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class BaseTest extends Utility {
+
+    String browser = PropertyReader.getInstance().getProperty("browser");
+
+    @BeforeMethod
+    public void setUp(){
+        selectBrowser(browser);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        closeBrowser();
+    }
+}
